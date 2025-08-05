d662e5113791b37130226d122d9d3317119db773
Subham Kumar <shubhamkr354@gmail.com>
2025-08-06 02:49:16 +0530

    added client

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import express, { Request, Response } from "express";
 import v1 from "./routes";
 import cors from "cors";
+import path from "path";
 const app = express();
 
 const port = process.env.PORT || 4000;
 app.use(cors()).use(express.json({ limit: "10mb" }));
+app.use(express.static(path.join(__dirname, "client/dist")));
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello i am from Patna");
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 
 app.use("/v1", v1);
